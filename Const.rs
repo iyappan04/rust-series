172d677cbe9b4c5f1e9c136d3e8c2aeda9ce9761
iyappan <iyappank165@gmail.com>
2024-04-01 16:28:54 +0530

    changes added.

---
+++ Const.rs
@@ -0,0 +1,16 @@
+
+const pi: f32 = 3.145; // no memory allocation 
+
+fn main(){
+
+   println!("Value of the PI is {}", pi);
+
+
+   println!("Value added into PI is {}", pi+3.0);
+
+   let x: i32 = 18; // The value is stored in stack while in runtime 
+
+
+   
+ 
+}
---
+++ Vector.rs
@@ -0,0 +1,5 @@
+fn main(){
+
+    // Grow and shrink at runtime. uses Heap memory. it's not fixed size
+
+}
